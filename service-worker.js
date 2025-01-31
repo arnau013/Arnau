db795ec41f92b49e1fc2e443520cb8265a8349c8
arnau013 <149940526+arnau013@users.noreply.github.com>
2025-01-31 09:16:48 +0000

    git commit -m "Descripción de los cambios"

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/1.html',
+                '/1.css',
+                '/1.js',
+                '/manifest.json',
+                '/icon.png'  // Asegúrate de tener el icono
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
